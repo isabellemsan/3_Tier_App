e2008263c7c629d131043f9fe3601a21ed1ee3e2
isabellemsan <isabelle.muresan@cloudreach.com>
2024-07-26 11:46:18 +0200

    created functions

--- App/app.py
+++ App/app.py
@@ -1,17 +1,41 @@
-from typing import Union
-
+from msilib.schema import table
 from fastapi import FastAPI
+import boto3
 
 app = FastAPI()
+dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+
+def scan_dynamodb_table(table_name: str):
+        table = dynamodb.Table(table_name)
+        response = table.scan()
+        items = response.get('Items', [])
+        return items
 
 
 @app.get("/cakes")
 def get_cakes():
-try: 
-    response = 
+    try:
+        table_name="CakeInventory"
+        response = scan_dynamodb_table(table_name)
+        items  = response.get('Items,[]')
+        cake_list = [{"cake_id": item["cake_id"], "name": item["name"]} for item in response]
+        return {"status": "success", "data": cake_list}
+    except Exception as e:
+            return {"status": "error", "message": str(e)}
 
 
-@app.get("/cake-descirption/{cake_id}")
+@app.get("/cake-descirption")
 def get_cakes_description():
-    try: 
-        response =
+        try: 
+            table_name="CakeInventory"
+            response = scan_dynamodb_table(table_name)
+            item = response.get('Item')
+            cake_descriptions = [{"cake_id": item["cake_id"], "name": item["name"], "description": item["description"]} for item in response]
+            return {"status": "success", "data": cake_descriptions}
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
